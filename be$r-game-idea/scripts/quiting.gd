af107a229b8f27bd7011794f0393a4f0ba351559
thorfinnking1 <157901908+thorfinnking1@users.noreply.github.com>
2025-06-12 22:10:50 +0300

    room 1 done

--- be$r-game-idea/scripts/quiting.gd
+++ be$r-game-idea/scripts/quiting.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+@onready var timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	timer.start(0.5)
+	await timer.timeout
 	get_tree().quit()
