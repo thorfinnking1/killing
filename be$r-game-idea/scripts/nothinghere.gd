0b97e728d508bbffbd4640e4aaee1bbaf5a2a19c
MrZece <domnulx7@gmail.com>
2025-06-15 11:31:56 +0300

    dvvdRGRWFWEWEXWXXWWXX

--- be$r-game-idea/scripts/nothinghere.gd
+++ be$r-game-idea/scripts/nothinghere.gd
@@ -1,4 +1,6 @@
 extends Node2D
 
+@export_file("*.tscn") var place:String
+
 func _on_timer_timeout() -> void:
-	get_tree().change_scene_to_file("res://scenes/mainMENU.tscn")
+	get_tree().change_scene_to_file(place)
